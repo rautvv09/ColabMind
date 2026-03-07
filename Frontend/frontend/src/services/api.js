ee9fee896056dfc716694d918d83d1c5efa7f924
Shivtej-Pawar <shivtejapawar5@gmail.com>
2026-03-07 09:58:53 +0530

    configure frontend API url

--- Frontend/frontend/src/services/api.js
+++ Frontend/frontend/src/services/api.js
@@ -5,8 +5,7 @@ import axios from "axios";
 ====================================================== */
 
 const BASE_URL =
-  import.meta.env.VITE_API_URL || "http://127.0.0.1:5000";
-
+  import.meta.env.VITE_API_URL || "https://colabmind.onrender.com";
 const api = axios.create({
   baseURL: BASE_URL,
   timeout: 30000,

89d28ac6a2e3e9805887fd154213d73f3e690b17
Rohan8598 <rohanpatil7248@gmail.com>
2026-03-07 10:37:34 +0530

    final

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -1,13 +1,16 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()   # <-- THIS LINE IS REQUIRED
 
 class Config:
+
     MONGO_URI = os.getenv("MONGO_URI")
     DB_NAME = os.getenv("DB_NAME", "instagram_db")
 
-    FLASK_DEBUG = os.getenv("FLASK_DEBUG", False)
+    FLASK_DEBUG = os.getenv("FLASK_DEBUG", True)
     PORT = int(os.getenv("PORT", 5000))
 
-    # Mongo collections
     COLLECTION_CREATORS = "profiles"
     COLLECTION_COLLABORATIONS = "collaborations"
     COLLECTION_BRANDS = "brands"

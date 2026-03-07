94ded372d836a2e74bec294ec8330e385e0f39c5
Shivtej-Pawar <shivtejapawar5@gmail.com>
2026-03-07 09:37:17 +0530

    fix missing collection config

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -3,5 +3,11 @@ import os
 class Config:
     MONGO_URI = os.getenv("MONGO_URI")
     DB_NAME = os.getenv("DB_NAME", "instagram_db")
+
     FLASK_DEBUG = os.getenv("FLASK_DEBUG", False)
     PORT = int(os.getenv("PORT", 5000))
+
+    # Mongo collections
+    COLLECTION_CREATORS = "profiles"
+    COLLECTION_COLLABORATIONS = "collaborations"
+    COLLECTION_BRANDS = "brands"
